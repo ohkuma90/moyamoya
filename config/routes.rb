c1b973c65f3fdc07c69617b1a1c00066ee945a2b
ohkuma90 <s.ohkuma001@gmail.com>
2023-09-17 16:51:23 +0900

    ルーティング・コントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "worries#index"
   resources :worries, only: [:index, :new, :create, :show, :destroy]
+  resources :todos, only: :index
   devise_for :users
 end
