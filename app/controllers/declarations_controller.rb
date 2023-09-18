b66849cf28373081104bde14cdff29d0583139b1
ohkuma90 <s.ohkuma001@gmail.com>
2023-09-18 20:46:30 +0900

    デストロイアクションの追加

--- app/controllers/declarations_controller.rb
+++ app/controllers/declarations_controller.rb
@@ -17,6 +17,16 @@ class DeclarationsController < ApplicationController
     end
   end
 
+  def destroy
+    @declaration = Declaration.find(params[:id])
+    if current_user.id == @declaration.user_id
+      @declaration.destroy
+      redirect_to user_path(current_user.id)
+    else
+      redirect_to root_path
+    end
+  end
+
   private
 
   def declaration_params
