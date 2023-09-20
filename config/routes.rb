56f9056b1311870e4cd366a4462a1aa648f77e34
ohkuma90 <s.ohkuma001@gmail.com>
2023-09-20 21:07:52 +0900

    ルートパスを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  root "worries#index"
+  root "welcome#index"
   resources :worries, only: [:index, :new, :create, :show, :destroy]
   resources :todos
   resources :declarations, only: [:index, :new, :create, :destroy]
+  resources :welcome, only: [:index]
   devise_for :users
   resources :users, only: :show
 end
